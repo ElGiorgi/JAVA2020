73840b3bfc2fa2076658852dd982b5fbcff22f92
georgedaw <59819570+ElGiorgi@users.noreply.github.com>
2021-05-24 22:49:45 +0200

    Update ejercicio1.java

--- JV2020/JV2020/src/practica7/ejercicio1.java
+++ JV2020/JV2020/src/practica7/ejercicio1.java
@@ -1,5 +1,36 @@
-package main.java;
 
 public class ejercicio1 {
+	private int X;
+	private int Y;
+	
+	public Punto(int X, int Y) {
+		setX(X);
+		setY(Y);
+	}
+	
+	public Punto() {
+		this(0,0);
+	}
+	
+	public Punto(Punto punto) {
+		setX(punto.getX());
+		setY(punto.getY());
+	}
+	
+	public int getX() {
+		return X;
+	}
+
+	public void setX(int x) {
+		X = x;
+	}
+
+	public int getY() {
+		return Y;
+	}
+
+	public void setY(int y) {
+		Y = y;
+	}
 
 }
