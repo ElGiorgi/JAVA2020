b54852a6f917c2890bc886785ecf54b81e10e5d9
georgedaw <59819570+ElGiorgi@users.noreply.github.com>
2021-05-24 22:50:33 +0200

    Update ejercicio2.java

--- JV2020/JV2020/src/practica7/ejercicio2.java
+++ JV2020/JV2020/src/practica7/ejercicio2.java
@@ -1,5 +1,39 @@
-package main.java;
+public class PuntoCartesiano {
 
-public class ejercicio2 {
+
+	private int CordenadaX;
+	private int CordenadaY;
+	
+	public PuntoCartesiano(int X, int Y) {
+		setX(X);
+		setY(Y);
+	}
+	
+	public PuntoCartesiano() {
+		this(0,0);
+	}
+	
+	public PuntoCartesiano(Punto punto) {
+		setX(punto.getX());
+		setY(punto.getY());
+	}
+	
+	public int getX() {
+		return CordenadaX;
+	}
+
+	public void setX(int x) {
+		CordenadaX = x;
+	}
+
+	public int getY() {
+		return CordenadaY;
+	}
+
+	public void setY(int y) {
+		CordenadaY = y;
+	}
+
+
 
 }
